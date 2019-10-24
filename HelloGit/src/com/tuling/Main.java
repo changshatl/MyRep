4bf4141d591f8ff2af67c971eb5136c50c663214
frd <19857897@qq.com>
2019-10-24 19:46:26 +0800

    项目修改了

--- HelloGit/src/com/tuling/Main.java
+++ HelloGit/src/com/tuling/Main.java
@@ -5,5 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.print("Hello Git");
+
+        for(int i=0;i<10;i++){
+            System.out.print(("hello")+i);
+        }
     }
 }
